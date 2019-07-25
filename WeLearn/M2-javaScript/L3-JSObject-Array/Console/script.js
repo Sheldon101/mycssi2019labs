2b89e8d65fc356016d9ad506a48e10f7cc6c1e67
Citlally Gomez <gomez466@csusm.edu>
2019-07-25 10:13:58 -0700

    Array lol

---
+++ WeLearn/M2-javaScript/L3-JSObject-Array/Console/script.js
@@ -0,0 +1,14 @@
+const article = {
+
+  name:"Dog family ",
+  views:1035,
+  datePublished: "03/25/18",
+  author:{
+    name: "J",
+    title: "K"
+  }
+};
+let x="1";
+let y="2";
+let z="3";
+const lol= [x,y,z];
