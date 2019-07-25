21ec35d9d5d9760f4f642c7d6e14fd613d66a3bd
Citlally Gomez <gomez466@csusm.edu>
2019-07-25 11:59:47 -0700

    Forloops

--- WeLearn/M2-javaScript/L3-JSObject-Array/Console/script.js
+++ WeLearn/M2-javaScript/L3-JSObject-Array/Console/script.js
@@ -8,7 +8,33 @@ const article = {
     title: "K"
   }
 };
+const friends ["M""K""L"];
+/*
+console.log();
+friends
+friends.push("C") adding c to the list
+friends
+("M","K","L","C")
+freinds.pop()   used to remove C
+  "C"
+  friends
+  ("M","K","L")
+
+*/
+/* classes.indexOf ("")  this is used to access to the index*/
 let x="1";
 let y="2";
 let z="3";
 const lol= [x,y,z];
+const classes = [];
+/*classes.push({course: "CS11", title:"CS1"});
+classes.push({course: "Math 160", title:"C1"});
+const party ={
+
+  date: "7/4/18",
+  location:"Library",
+  guest:["M","J","S"]
+};
+
+
+party .guest [0]*/
---
+++ WeLearn/M2-javaScript/L4-JSLoop-DOM/loop/script.js
@@ -0,0 +1,25 @@
+/*
+const names = ["L","K","M","Q","W"];
+
+for(let c= 0; c<5; c++)
+{
+console.log(names[c]);
+}
+*/
+
+/*
+console.log( names[0]);
+console.log( names[1]);
+console.log( names[2]);
+console.log( names[3]);
+console.log( names[4]);
+*/
+/* console.log(names.join("",""));
+*/
+let sum = 0;
+let numbers= [1,2,3,4,5,6,7,8,9,10];
+const findTotal= ((item) =>{
+  sum= sum+item;
+  console.log(sum);
+
+});
