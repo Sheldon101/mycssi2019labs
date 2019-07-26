38776e0ae0c5d3688cce34406545148aa2400fd1
Citlally Gomez <gomez466@csusm.edu>
2019-07-26 10:32:40 -0700

    adding to python

--- WeLearn/M3-Python/L1-Python_Intro/mysum.py
+++ WeLearn/M3-Python/L1-Python_Intro/mysum.py
@@ -1,10 +1,18 @@
-for i in range(5):
-    print(i)
+#for i in range(5):
+    #print(i)
+
+# def findSum(x):
+#     y=0
+#     for i in range(x): # x is ten
+#
+#        y = y + i    # y is the range of values
+#     print(y)
+# #findSum(10)
+# findSum(20)
 
 def findSum(x):
-    y=0
+    sum=0
     for i in range(x):
-       y = y + i
-    print(y)
-#findSum(10)
-findSum(20)
+      sum=sum+1
+ return sum+x # add the last integer
+print (findSum(20))
