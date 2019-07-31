06711807ce982f9ec33c4141edcbfaa85f6245fa
Citlally Gomez <gomez466@csusm.edu>
2019-07-31 11:59:14 -0700

    stage 4

---
+++ WeLearn/M3-Python/L2-Python_List_Dict/list.py
@@ -0,0 +1,47 @@
+#students = ["A", "J" ,"Da"]
+#students.append("De")
+#print(students)
+
+#students = ["A", "J" ,"D"]
+#students.insert(1,"Z") goes 2nd
+#print(students)
+
+#students = ["A", "J" ,"D", "J"]
+#students.remove("Javi") only the 1st will be removed
+#print(students)
+
+#my_list.insert(index,element)
+
+#smith_siblings = ['E','M','G','A','C','D']
+#for name in smith_siblings:
+     #print(name + " Smith")
+
+
+
+# smith_siblings= ['E','M','G',"B","J"]
+# for index in range(len(smith_siblings)):
+#     smith_siblings[index]= smith_siblings[index] + "Smith"
+# print(smith_siblings)
+# print index
+
+
+# names= ['R','B','A','S','J','R']
+# for i in range (len(name)):
+#     if names[i]=="J":
+#         names[i]+= "Sn"
+#     else:
+#         names[i] += "St"
+# print (names)
+
+# superh = ['CM','Wonder Women','S','KK','B','JJ']
+# demoted_h = str(raw_input("E"))
+# if demoted_h in superh:
+#     superh.remove(demoted_h)
+#     print("T", superh)
+# else:
+#    print("hero not here")
+#
+#names= ['R','B','A','S','J','R']
+#names[::-1]
+#reverses the list
+# name [4:2:-1] goes to John and back to A includes S and J the -1 flips them
